480b571ec2a6fcb512d6b4bea6b0f295924dcdf2
srichaitanyapalem05 <srichaitanyapalem05@gmail.com>
2026-04-04 20:33:11 +0530

    Initial commit

---
+++ src/main/java/com/example/zorvyn/ZorvynApplication.java
@@ -0,0 +1,13 @@
+package com.example.zorvyn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZorvynApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZorvynApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/zorvyn/ZorvynApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.zorvyn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZorvynApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
